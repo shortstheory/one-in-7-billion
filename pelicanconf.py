65fa5a68e87bccdcdcc8223532408bd8d8b29364
arnav dhamija <arnav.dhamija@gmail.com>
2017-02-01 21:28:42 +0530

    Changed URL

--- pelicanconf.py
+++ pelicanconf.py
@@ -4,7 +4,7 @@ from __future__ import unicode_literals
 
 AUTHOR = u'Arnav Dhamija'
 SITENAME = u'One in 7 Billion'
-SITEURL = ''#'https://shortstheory.github.io'
+SITEURL = 'https://shortstheory.github.io'
 
 TIMEZONE = 'Asia/Calcutta'
 
